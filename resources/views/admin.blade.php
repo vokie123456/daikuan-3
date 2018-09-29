e57a425c68a0d2947deb8842a1a744a5ff4f88ac
linzy120 <linzy120@126.com>
2018-09-29 12:47:18 +0800

    网站名称可配置

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -7,7 +7,7 @@
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>真要贷后台</title>
+    <title>{{ config('app.name') }}</title>
 
     <style type="text/css">
         #example {
--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>真要贷后台</title>
+    <title>{{ config('app.name') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -21,7 +21,7 @@
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
                 <a class="navbar-brand" href="#">
-                    真要贷后台
+                    {{ config('app.name') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
